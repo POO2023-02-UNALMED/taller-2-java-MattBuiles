34f7c40c31862dc05329014b975572060144fd7f
MattBuiles <141962207+MattBuiles@users.noreply.github.com>
2023-08-18 22:54:06 -0500

    Update Auto.java

--- src/main/java/Auto.java
+++ src/main/java/Auto.java
@@ -18,9 +18,14 @@ public class Auto{
     String verificarIntegridad(){
       int b=0;
       for (int i=0;i<asientos.length;i++){
-        if (asientos[i].registro!=motor.registro)
-          b++;
-      }
+        if(asientos[i] instanceof Asiento)
+          if (asientos[i].registro!=motor.registro)
+            b++;
+          else if (asientos[i].registro!=this.registro)
+            b++;
+          else if (motor.registro!=this.registro)
+            b++;
+        }
       if (b>0)
         return("Las piezas no son originales");
       else
