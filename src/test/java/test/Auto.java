a015729127a8dc0b8ec72d85eee95fcac5250dd0
MattBuiles <141962207+MattBuiles@users.noreply.github.com>
2023-08-18 22:09:40 -0500

    Corrección if verificarIntegridad

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -21,6 +21,10 @@ public class Auto{
       for (int i=0;i<asientos.length;i++){
         if (asientos[i].registro!=motor.registro)
           b++;
+        else if (asientos[i].registro!=this.registro)
+          b++;
+        else if (motor.registro!=this.registro)
+          b++;
       }
       if (b>0)
         return("Las piezas no son originales");
