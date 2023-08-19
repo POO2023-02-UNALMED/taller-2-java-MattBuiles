307214bd01abee9503dedec805ce6afb8f720863
MattBuiles <141962207+MattBuiles@users.noreply.github.com>
2023-08-18 21:55:36 -0500

    Desarrollo de los atributos y métodos

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,30 @@
+package test;
+public class Auto{
+    String modelo;
+    int precio;
+    Asiento[] asientos;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
+
+    int cantidadAsientos(){
+      int a=0;
+      for (int i=0;i<asientos.length;i++){
+        if (asientos[i] instanceof Asiento)
+          a++;
+      }
+      return a;
+    }
+    String verificarIntegridad(){
+      int b=0;
+      for (int i=0;i<asientos.length;i++){
+        if (asientos[i].registro!=motor.registro)
+          b++;
+      }
+      if (b>0)
+        return("Las piezas no son originales");
+      else
+        return("Auto original");
+    }
+}
